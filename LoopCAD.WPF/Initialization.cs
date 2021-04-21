2f65db9cc3a714b6b4264d832bb98bc126ff1a2c
Marc Durham <marcdurham@gmail.com>
2021-04-21 10:47:51 -0700

    Split labeling into two commands

--- LoopCAD.WPF/Initialization.cs
+++ LoopCAD.WPF/Initialization.cs
@@ -55,31 +55,20 @@ namespace LoopCAD.WPF
 
         }
 
-        [CommandMethod("LABELNODES")]
+        [CommandMethod("LABEL-NODES")]
         public void LabelNodesCommand()
         {
-            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
-            ed.WriteMessage("\nLabeling nodes...");
+            Editor editor = Application.DocumentManager.MdiActiveDocument.Editor;
+            editor.WriteMessage("\nLabeling nodes...");
 
             Database db = HostApplicationServices.WorkingDatabase;
             Transaction trans = db.TransactionManager.StartTransaction();
-
-            var headLabeler = new Labeler(trans, "HEADNUMBER", "HeadLabel2", "HeadLabels", ColorIndices.Magenta);
-            var teeLabeler = new Labeler(trans, "TEENUMBER", "TeeLabel2", "TeeLabels", ColorIndices.Green);
-            var pipeLabeler = new Labeler(trans, "PIPENUMBER", "PipeLabel2", "PipeLabels", ColorIndices.Blue)
-            { 
-                TextHeight = 3.0,
-                XOffset = 0.0,
-                YOffset = 0.0,
-                HorizontalMode = TextHorizontalMode.TextCenter 
-            };
-
-
             BlockTable blkTbl = trans.GetObject(db.BlockTableId, OpenMode.ForWrite) as BlockTable;
             BlockTableRecord modelSpace = trans.GetObject(blkTbl[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
 
-            int pipeNumber = 1;
-            int nodeNumber = 1;
+            var headLabeler = new Labeler(trans, "HEADNUMBER", "HeadLabel2", "HeadLabels", ColorIndices.Magenta);
+            var teeLabeler = new Labeler(trans, "TEENUMBER", "TeeLabel2", "TeeLabels", ColorIndices.Green);
+
             int headNumber = 1;
             int teeNumber = 1;
             int riserNumber = 1;
@@ -90,7 +79,6 @@ namespace LoopCAD.WPF
                     var block = trans.GetObject(objectId, OpenMode.ForRead) as BlockReference;
 
                     headLabeler.CreateLabel($"H.{headNumber++}", block.Position);
-                    //CreatePipeLabel(trans, pipeNumber, block.Position);
                 }
                 else if (IsTee(trans, objectId))
                 {
@@ -98,7 +86,36 @@ namespace LoopCAD.WPF
 
                     teeLabeler.CreateLabel($"T.{teeNumber++}", block.Position);
                 }
-                else if (IsPipe(trans, objectId))
+            }
+
+            editor.WriteMessage($"\n{headNumber} heads labeled.");
+            editor.WriteMessage($"\n{teeNumber} tees labeled.");
+            trans.Commit();
+        }
+
+        [CommandMethod("LABEL-PIPES")]
+        public void LabelPipesCommand()
+        {
+            Editor editor = Application.DocumentManager.MdiActiveDocument.Editor;
+            editor.WriteMessage("\nLabeling pipes...");
+
+            Database db = HostApplicationServices.WorkingDatabase;
+            Transaction trans = db.TransactionManager.StartTransaction();
+            BlockTable blkTbl = trans.GetObject(db.BlockTableId, OpenMode.ForWrite) as BlockTable;
+            BlockTableRecord modelSpace = trans.GetObject(blkTbl[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+
+            var pipeLabeler = new Labeler(trans, "PIPENUMBER", "PipeLabel2", "PipeLabels", ColorIndices.Blue)
+            {
+                TextHeight = 3.0,
+                XOffset = 0.0,
+                YOffset = 0.0,
+                HorizontalMode = TextHorizontalMode.TextCenter
+            };
+
+            int pipeNumber = 1;
+            foreach (var objectId in modelSpace)
+            {
+                if (IsPipe(trans, objectId))
                 {
                     var polyline = trans.GetObject(objectId, OpenMode.ForRead) as Polyline;
 
@@ -107,7 +124,7 @@ namespace LoopCAD.WPF
                         Point3d lastVertex = polyline.GetPoint3dAt(i - 1);
                         Point3d vertex = polyline.GetPoint3dAt(i);
                         pipeLabeler.CreateLabel(
-                            text: $"p{pipeNumber}", 
+                            text: $"p{pipeNumber}",
                             position: Midpoint(vertex, lastVertex));
                     }
 
@@ -116,6 +133,7 @@ namespace LoopCAD.WPF
             }
 
             trans.Commit();
+            editor.WriteMessage($"\n{pipeNumber} pipes labeled.");
         }
 
         bool IsHead(Transaction trans, ObjectId objectId)
@@ -147,37 +165,6 @@ namespace LoopCAD.WPF
                 x: (b.X - a.X) / 2 + a.X,
                 y: (b.Y - a.Y) / 2 + a.Y,
                 z: 0);
-        }
-
-        [CommandMethod("LABELPIPES")]
-        public void LabelPipesCommand()
-        {
-            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
-            ed.WriteMessage("\nLabeling nodes...");
-
-            Database db = HostApplicationServices.WorkingDatabase;
-            Transaction trans = db.TransactionManager.StartTransaction();
-
-            dynamic bt = db.BlockTableId;
-            string str = "ABC";
-
-            var textEnts =
-                from btrs in (IEnumerable<dynamic>)bt
-                from ent in (IEnumerable<dynamic>)btrs
-                where
-                ((ent.IsKindOf(typeof(DBText)) &&
-                    (ent.TextString.Contains(str))) ||
-                (ent.IsKindOf(typeof(MText)) &&
-                    (ent.Contents.Contains(str))))
-                select ent;
-
-            Point3d pnt1 = new Point3d(0, 0, 0);
-            Point3d pnt2 = new Point3d(10, 10, 0);
-
-            Line lineObj = new Line(pnt1, pnt2);
-
-            trans.AddNewlyCreatedDBObject(lineObj, true);
-            trans.Commit();
         }
     }
 }
